e4a46fecb5aa33c11d821e615259e6cf6d5d63fd
Boston <BostonCEssary@gmail.com>
2023-09-26 09:10:18 -0700

    add media queries to have gameboard fit correctly on mobile screns. In Process of replacing alert messages with modal messges

--- main.js
+++ main.js
@@ -2,8 +2,6 @@ const playerFactory = (name, piece) => {
     const getName = () => {return name}
     const getPiece = () => {return piece}
 
-    
-
     return {getName, getPiece}
 }
 
@@ -18,7 +16,27 @@ const GameBoard = () => {
         return currentBoard[index]
     }
 
-    return {logicBoard, placePiece, getPlacedPiece}
+    const getXPieces = (currentBoard) => {
+        const xPieceIndexes = []
+        for(i = 0; i < currentBoard.length; i++){
+            if(currentBoard[i] == "X"){
+                xPieceIndexes.push(i)
+            }
+        }
+        return xPieceIndexes
+    }
+
+    const getOPieces = (currentBoard) => {
+        const oPieceIndexes = []
+        for(i = 0; i < currentBoard.length; i++){
+            if(currentBoard[i] == "O"){
+                oPieceIndexes.push(i)
+            }
+        }
+        return oPieceIndexes
+    }
+
+    return {logicBoard, placePiece, getPlacedPiece, getXPieces, getOPieces}
 }
 
 const displayController = () => {
@@ -56,43 +74,38 @@ const Game = () => {
     let game = GameBoard();
     let board = game.logicBoard
     let controller = displayController()
-   
     controller.drawBoard(board)
     let currentPlayer = player1;
     let spots = document.getElementsByClassName("spot")
     let winningCombos = [[0,1,2], [0,3,6], [0,4,8], [1,4,7], [2,5,8],[2, 4, 6] ,[3, 4, 5], [6,7,8]]
+    let alertModal = document.getElementById("alert-modal")
 
-    
-    
     let checker = (arr, target) => target.every(v => arr.includes(v));
     
-
+    const checkForTie = (currentBoard) => {
+        if(roundCount >= 9){
+            alert("game is a tie")
+        }
+    }
     const checkForWinner = (currentBoard) => {
-        let xPieceLocations = []
-        let oPieceLocations = []
-        for(i = 0; i < currentBoard.length; i++){
-            if (currentBoard[i] == "X"){
-                xPieceLocations.push(i)
-            }
-            else if (currentBoard[i] == "O"){
-                oPieceLocations.push(i)
-            }
-            else{
-                break
-            }
-        }
-        for (i=0; i < winningCombos.length; i++){
-            console.log(checker(xPieceLocations, winningCombos[i]))
-
-        }
-        console.log(xPieceLocations)
-        console.log(oPieceLocations)
+        let xPieceLocations = game.getXPieces(board)
+        let oPieceLocations = game.getOPieces(board)
         
-       
+        for (i=0; i < winningCombos.length; i++){
+            if (checker(xPieceLocations, winningCombos[i])) {
+                alertModal.textContent = "Player 1 has won"
+                alertModal.classList.remove("hidden")
+            }
+            else if (checker(oPieceLocations, winningCombos[i])) {
+                alert("Player 2 has won")
+            }
+        }
     }
 
     const switchRound = () => {
+        console.log(roundCount)
         checkForWinner(board);
+        checkForTie()
         if (roundCount % 2 === 0){
             currentPlayer = player1
         }
@@ -114,27 +127,13 @@ const Game = () => {
                 }
                 else {
                     alert("Spot Taken")
-                }
-               
-                    
-                
-            
-                    
-                    
-                
-
-                
-                
-                
-                console.log(board)
-                
+                }    
             })
-            console.log("blah")
         }
     }
 
-    
     addClickHandler(board)
+    
 }
 
 Game()
