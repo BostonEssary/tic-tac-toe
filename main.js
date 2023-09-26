301201606860bc87d0dfbfe6ca3d667f428ee3fa
Boston <bostoncessary@gmail.com>
2023-09-25 22:00:36 -0700

    add logic to prevent users from using a taken. Next is to refining win logic

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const playerFactory = (name, piece) => {
     const getName = () => {return name}
     const getPiece = () => {return piece}
 
+    
+
     return {getName, getPiece}
 }
 
@@ -12,7 +14,11 @@ const GameBoard = () => {
         currentBoard[spot] = playerPiece
     }
 
-    return {logicBoard, placePiece}
+    const getPlacedPiece = (index, currentBoard) => {
+        return currentBoard[index]
+    }
+
+    return {logicBoard, placePiece, getPlacedPiece}
 }
 
 const displayController = () => {
@@ -27,7 +33,7 @@ const displayController = () => {
             div.id = `spot-${count}`
             div.classList.add("spot")
             if(Number.isInteger(piece)){
-                div.textContent = "test"
+                div.textContent = null
             }
             else {
                 div.textContent = piece
@@ -50,25 +56,43 @@ const Game = () => {
     let game = GameBoard();
     let board = game.logicBoard
     let controller = displayController()
-    let switchUserButton = document.createElement("button")
-    switchUserButton.textContent = "switch player"
-    document.body.append(switchUserButton)
+   
     controller.drawBoard(board)
     let currentPlayer = player1;
     let spots = document.getElementsByClassName("spot")
+    let winningCombos = [[0,1,2], [0,3,6], [0,4,8], [1,4,7], [2,5,8],[2, 4, 6] ,[3, 4, 5], [6,7,8]]
 
-    switchUserButton.addEventListener("click", (e) => {
-        currentPlayer = player1
-    })
     
     
+    let checker = (arr, target) => target.every(v => arr.includes(v));
+    
 
-    const checkForWinner = () => {
+    const checkForWinner = (currentBoard) => {
+        let xPieceLocations = []
+        let oPieceLocations = []
+        for(i = 0; i < currentBoard.length; i++){
+            if (currentBoard[i] == "X"){
+                xPieceLocations.push(i)
+            }
+            else if (currentBoard[i] == "O"){
+                oPieceLocations.push(i)
+            }
+            else{
+                break
+            }
+        }
+        for (i=0; i < winningCombos.length; i++){
+            console.log(checker(xPieceLocations, winningCombos[i]))
+
+        }
+        console.log(xPieceLocations)
+        console.log(oPieceLocations)
         
+       
     }
 
     const switchRound = () => {
-        
+        checkForWinner(board);
         if (roundCount % 2 === 0){
             currentPlayer = player1
         }
@@ -80,10 +104,28 @@ const Game = () => {
     const addClickHandler = () => {
         for(i = 0; i < spots.length; i++){
             spots[i].addEventListener("click", (e) => {
-                e.target.textContent = currentPlayer.getPiece()
-                game.placePiece(currentPlayer.getPiece(), e.target.dataset.spot, board )
-                switchRound()
-                roundCount++
+                let data = e.target.dataset.spot
+                console.log(data)
+                if (Number.isInteger(board[data])){
+                    e.target.textContent = currentPlayer.getPiece()
+                    game.placePiece(currentPlayer.getPiece(), e.target.dataset.spot, board )
+                    switchRound()
+                    roundCount++
+                }
+                else {
+                    alert("Spot Taken")
+                }
+               
+                    
+                
+            
+                    
+                    
+                
+
+                
+                
+                
                 console.log(board)
                 
             })
@@ -92,11 +134,7 @@ const Game = () => {
     }
 
     
-    addClickHandler()
-    
-    
-    
-    
+    addClickHandler(board)
 }
 
 Game()
